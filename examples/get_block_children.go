6909e09128e1968f6aa178a6f6d418fe00218129
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2021-05-16 20:09:51 -0700

    show info about blocks in the example

--- examples/get_block_children.go
+++ examples/get_block_children.go
@@ -7,11 +7,35 @@ import (
 	"github.com/kjk/notion"
 )
 
+func showBlock(b *notion.Block) {
+	logf("  %s %s, has_children: %v\n", b.Type, b.ID, b.HasChildren)
+	switch b.Type {
+	case notion.BlockTypeParagraph:
+		logf(" %v\n", b.Paragraph.Text)
+	case notion.BlockTypeHeading1:
+		logf(" %v\n", b.Heading1.Text)
+	case notion.BlockTypeHeading2:
+		logf(" %v\n", b.Heading2.Text)
+	case notion.BlockTypeHeading3:
+		logf(" %v\n", b.Heading3.Text)
+	case notion.BlockTypeBulletedListItem:
+		logf(" %v\n", b.BulletedListItem.Text)
+	case notion.BlockTypeNumberedListItem:
+		logf(" %v\n", b.NumberedListItem.Text)
+	case notion.BlockTypeToDo:
+		logf(" %v\n", b.ToDo.Text)
+	case notion.BlockTypeToggle:
+		logf(" %v\n", b.Toggle.Text)
+	case notion.BlockTypeChildPage:
+	case notion.BlockTypeUnsupported:
+	}
+}
+
 func showBlockChildren(bcr *notion.BlockChildrenResponse) {
 	logf("showBlockChildren:\n")
 	logf("  %d children:\n", len(bcr.Results))
 	for _, b := range bcr.Results {
-		logf("  %v\n", b)
+		showBlock(&b)
 	}
 }
 
@@ -31,10 +55,9 @@ func getBlockChildren2(apiKey string, blockID string) {
 
 func getBlockChildren(apiKey string, blockID string) {
 	if blockID == "" {
-		// show info about regular page
-		getBlockChildren2(apiKey, "0367c2db381a4f8b9ce360f388a6b2e3")
-		// show info about page in a database
-		getBlockChildren2(apiKey, "e56b74a6398a43848137cca2a0de20b2")
+		// a test page https://www.notion.so/Test-all-blocks-c969c9455d7c4dd79c7f860f3ace6429
+		// with all block types
+		getBlockChildren2(apiKey, "c969c9455d7c4dd79c7f860f3ace6429")
 	} else {
 		getBlockChildren2(apiKey, blockID)
 	}
